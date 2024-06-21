236cc64301b256a3f020bc6c887954fa93148aa2
Tory009 <148058802+Tory009@users.noreply.github.com>
2024-06-21 17:31:43 +0500

    add lesson 79

---
+++ Lesson 79 js Условия/main.js
@@ -0,0 +1,18 @@
+
+const myAge = 26
+let result = null
+
+if (myAge >= 18) {
+result = 'Вы совершеннолетний'
+console.log(result)
+} else {
+result = 'Вам еще нет 18'
+console.log(result)
+}
+
+// ==================================================
+
+// const myAge = 26
+
+// myAge >= 18 ? console.log('Вы совершеннолетний') : console.log('Вам еще нет 18')
+
